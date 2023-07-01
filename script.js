7640966a44faf9d69ad0dac4901dc29f46627f17
billmukenga01 <billmukenga01@gmail.com>
2023-07-01 19:58:34 +0300

    added event listeners and linked script file

---
+++ script.js
@@ -0,0 +1,15 @@
+const container = document.querySelector('.gameboard')
+    .addEventListener('click',(e) => {
+        if(! e.target.classList.contains('gameboard')){
+            console.log('bill')
+        }
+    })
+;
+const GameBoard = (()=>{
+    const gameboard = [];
+    return{}
+})();
+
+const GameController = ()=>{
+    return{}
+}
